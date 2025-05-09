18da5241f846c7210024649024def4be9cc26666
Andreas Holmberg <andreas.holmberg@digitalengine.com>
2025-05-09 14:27:02 +0200

    fixing css and layout

--- src/app/components/game-menu/game-menu.component.ts
+++ src/app/components/game-menu/game-menu.component.ts
@@ -1,37 +1,45 @@
-import { Component, computed, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
 import { GameService } from '../../services/game.service';
-import { CharacterCreationComponent } from "../character-creation/character-creation.component";
+import { CharacterCreationComponent } from '../character-creation/character-creation.component';
 
 @Component({
-    selector: 'app-game-menu',
-    standalone: true,
-    imports: [CharacterCreationComponent],
-    templateUrl: './game-menu.component.html',
-    styleUrl: './game-menu.component.css'
+  selector: 'app-game-menu',
+  standalone: true,
+  imports: [CharacterCreationComponent],
+  templateUrl: './game-menu.component.html',
+  styleUrl: './game-menu.component.css',
 })
-export class GameMenuComponent implements OnInit{
-    @Output() startGame = new EventEmitter();
-    private gameService = inject(GameService);
-    ifCreatePlayerCharacter: boolean = false;
-   
-   ngOnInit(): void {
-       this.generateEnemies(3);
-   }
-    
-    generatePlayerCharacters(){
-        this.ifCreatePlayerCharacter = !this.ifCreatePlayerCharacter;
-    }
+export class GameMenuComponent implements OnInit {
+  @Output() startGame = new EventEmitter();
+  private gameService = inject(GameService);
+  ifCreatePlayerCharacter: boolean = false;
+  isOpen = false;
 
-    generateEnemies(number: number){
-        this.gameService.generateEnemies(number);
-        console.log(this.gameService.enemyCharacters());
-    }
+  ngOnInit(): void {
+    this.generateEnemies(3);
+  }
 
-    onStartGame(){
-        if (this.gameService.playerCharacters().length > 0) {
-            this.startGame.emit();
-        }else{
-            console.log("Have to create your team first.");
-        }
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  generatePlayerCharacters() {
+     this.isOpen = false;
+    this.ifCreatePlayerCharacter = !this.ifCreatePlayerCharacter;
+  }
+
+  generateEnemies(number: number) {
+    this.gameService.generateEnemies(number);
+    this.isOpen = false;
+    console.log(this.gameService.enemyCharacters());
+  }
+
+  onStartGame() {
+     this.isOpen = false;
+    if (this.gameService.playerCharacters().length > 0) {
+      this.startGame.emit();
+    } else {
+      console.log('Have to create your team first.');
     }
+  }
 }
